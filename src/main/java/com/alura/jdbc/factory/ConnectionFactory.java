da23d53ba95daf4200874830627176d187252b12
MolinAnimation <Molinanimation@gmail.com>
2023-09-13 18:19:57 -0500

    pool Connections

--- src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
+++ src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
@@ -4,10 +4,25 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 public class ConnectionFactory {
 
+    private DataSource dataSource;
+
+    public ConnectionFactory() {
+        var pooledDataSource = new ComboPooledDataSource();
+        pooledDataSource.setJdbcUrl("jdbc:mysql://localhost/control_de_stock?useTimezone=true&serverTimeZone=UTC");
+        pooledDataSource.setUser("root");
+        pooledDataSource.setPassword(".MAZdaRx7.");
+        pooledDataSource.setMaxPoolSize(10);
+
+        this.dataSource = pooledDataSource;
+    }
+
     public Connection recuperaConexion() throws SQLException {
-        return DriverManager.getConnection(
-                "jdbc:mysql://localhost/control_de_stock?useTimezone=true&serverTimeZone=UTC", "root", ".MAZdaRx7.");
+        return this.dataSource.getConnection();
     }
 }
---
+++ src/main/java/com/alura/jdbc/pruebas/PruebapoolDeConexiones.java
@@ -0,0 +1,16 @@
+package com.alura.jdbc.pruebas;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import com.alura.jdbc.factory.ConnectionFactory;
+
+public class PruebapoolDeConexiones {
+    public static void main(String[] args) throws SQLException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        for (int i = 0; i < 20; i++) {
+            Connection conexion = connectionFactory.recuperaConexion();
+
+            System.out.println("Abriendo la conexion numero" + (i + 1));
+        }
+    }
+}
