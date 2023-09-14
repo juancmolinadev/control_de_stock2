798e291b2a0d9b85ddef096dad79354ac5817237
MolinAnimation <Molinanimation@gmail.com>
2023-09-14 16:32:12 -0500

    Funciona

--- src/main/java/com/alura/jdbc/controller/ProductoController.java
+++ src/main/java/com/alura/jdbc/controller/ProductoController.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.alura.jdbc.factory.ConnectionFactory;
+import com.alura.jdbc.modelo.Producto;
 
 public class ProductoController {
 
@@ -79,52 +80,44 @@ public class ProductoController {
 		}
 	}
 
-	public void guardar(Map<String, String> producto) throws SQLException {
-		String nombre = producto.get("NOMBRE");
-		String descripcion = producto.get("DESCRIPCION");
-		Integer cantidad = Integer.valueOf(producto.get("CANTIDAD"));
-		Integer maximoCantidad = 50;
+	public void guardar(Producto producto) throws SQLException {
 
 		final Connection con = new ConnectionFactory().recuperaConexion();
 		con.setAutoCommit(false);
 
 		try (con) {
+			con.setAutoCommit(false);
+
 			final PreparedStatement statement = con.prepareStatement(
 					"INSERT INTO producto (NOMBRE, descripcion, cantidad) VALUES(?,?,?)",
 					Statement.RETURN_GENERATED_KEYS);
 
 			try (statement) {
-				do {
-					int cantidadParaGuardar = Math.min(cantidad, maximoCantidad);
+				ejecutarRegistro(producto, statement);
 
-					ejecutarRegistro(nombre, descripcion, cantidadParaGuardar, statement);
-
-					cantidad -= maximoCantidad;
-
-				} while (cantidad > 0);
 				con.commit();
 			} catch (Exception e) {
 				con.rollback();
 			}
-
 		}
 	}
 
-	public void ejecutarRegistro(String nombre, String descripcion, Integer cantidad, PreparedStatement statement)
+	public void ejecutarRegistro(Producto producto, PreparedStatement statement)
 			throws SQLException {
 
-		statement.setString(1, nombre);
-		statement.setString(2, descripcion);
-		statement.setInt(3, cantidad);
+		statement.setString(1, producto.getNombre());
+		statement.setString(2, producto.getDescripcion());
+		statement.setInt(3, producto.getCantidad());
 
 		statement.execute();
 
 		final ResultSet resultSet = statement.getGeneratedKeys();
 		try (resultSet) {
 			while (resultSet.next()) {
+				producto.setId(resultSet.getInt(1));
 				System.out.println(
 						String.format(
-								"fue insertado el producto de id", resultSet.getInt(1)));
+								"fue insertado el producto %s", producto));
 			}
 		}
 	}
